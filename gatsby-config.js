0c1b888f9576fa8900306a66e9a7f37e45a2929c
Dhaiwat Pandya <dhaiwatpandya@gmail.com>
2021-04-13 15:07:08 +0530

    Initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,12 @@
 module.exports = {
   siteMetadata: {
     // edit below
-    title: `Gatsby Starter Personal Blog`,
-    author: `Gatsby`,
+    title: `Dhaiwat Pandya`,
+    author: `Dhaiwat Pandya`,
     description: `A starter personal blog with styled components, dark mode, and Netlify CMS.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+    siteUrl: `https://blog.dhaiwatpandya.com/`,
     social: {
-      twitter: `gatsbyjs`,
+      twitter: `dhaiwat10`,
     },
   },
   plugins: [
